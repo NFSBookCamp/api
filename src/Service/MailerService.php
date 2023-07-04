965782c0eca3e76b5fba9570ac82369708b94208
Aliseth <charlotte.saidi@outlook.fr>
2023-07-05 00:04:13 +0200

    feat(SPR-10): (wip) met en place la réinitialisation de mot de passe

--- src/Service/MailerService.php
+++ src/Service/MailerService.php
@@ -6,21 +6,23 @@ use App\Entity\User;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\BodyRendererInterface;
 
 class MailerService
 {
-    public function __construct(private MailerInterface $mailer)
+    public function __construct(private MailerInterface $mailer, private BodyRendererInterface $bodyRenderer)
     {}
 
     public function sendResetPasswordEmail(User $user, string $urlLink)
     {
         $email = (new TemplatedEmail())
             ->to(new Address($user->getEmail()))
-            ->subject('T\as zappé ton mot de passe et t\'as demandé à le changer')
+            ->subject('Vous avez oublié votre mot de passe')
             ->htmlTemplate('email/reset_password_email.html.twig')
             ->context([
                 'urlLink' => $urlLink,
             ]);
+        $this->bodyRenderer->render($email);
         $this->mailer->send($email);
     }
 }
